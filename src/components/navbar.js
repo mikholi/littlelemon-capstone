70b39f4ad90bab869f3e99cd3bb3ea14bcc77e3e
mikholi <keranemi@icloud.com>
2024-10-31 14:42:52 +0200

    navbar started

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,21 +1,32 @@
 import style from './navbar.css';
-import Logo from '../../public/images/Logo.svg'
-
-
-
+import Logo from './../images/Logo.svg';
+import React from 'react';
+// Import Link if using react-router for navigation
+// import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-    return(
+    return (
         <header>
-            <nav class={style.Navbar}>
-                <img src={Logo} alt='logo' class={style.Logo} />
-                <ul class={style.NavLinks}>
+            <nav className="Navbar">
+                <img src={Logo} alt="logo" className="Logo" />
+                <ul className="NavLinks">
                     <li>
-                        <Link to="/">Home</Link>
+                        <a href="#">About</a>
+                    </li>
+                    <li>
+                        <a href="#">Menu</a>
+                    </li>
+                    <li>
+                        <a href="#">Order online</a>
+                    </li>
+                    <li>
+                        <a href="#">Login</a> Tänne vielä linkkejä
                     </li>
                 </ul>
             </nav>
-
         </header>
-    )
-}
+    );
+};
+
+
+export default Navbar;
